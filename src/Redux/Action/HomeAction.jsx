a96b5a817dc0920a0be78f7b8c4a51c61df5fe5f
nurfaidzy <nurfaidzy@gmail.com>
2022-09-16 10:47:23 +0700

    Clear n works

--- src/Redux/Action/HomeAction.jsx
+++
@@ -1,9 +0,0 @@
-export const PROSES_DATA = "PROSES_DATA";
-export const KUISIKAN = "KUISIKAN";
-export const GAGALLL = "GAGALLL";
-
-export const prosesdata = () => {
-  return {
-    type: PROSES_DATA,
-  };
-};
--- src/Redux/Root/Store.jsx
+++ src/Redux/Root/Store.jsx
@@ -2,14 +2,13 @@ import { applyMiddleware, combineReducers, createStore } from "redux";
 import createSagaMiddleware from "@redux-saga/core";
 import { composeWithDevTools } from "redux-devtools-extension";
 
-import Tampildata from "./../Reducer/Tampildata";
 import TampilPost from "../Reducer/Tampilpost";
 
 import Tampilsaga from "./../Saga/Tampilsaga";
 
 const sagaMiddleware = createSagaMiddleware();
 
-const rootReducer = combineReducers({ Tampildata, TampilPost });
+const rootReducer = combineReducers({ TampilPost });
 
 const store = createStore(
   rootReducer,
